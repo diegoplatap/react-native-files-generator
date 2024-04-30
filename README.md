1312bf2814c74b6d2fab341940b967533f8c45fa
Diego Plata <diego.plata@globant.com>
2024-04-30 15:40:36 -0500

    create main functionalities

--- README.md
+++ README.md
@@ -1,71 +1,51 @@
-# dp-vs-code-rn-fast-file-generator README
+# Screen and View Generator for React Native
 
-This is the README for your extension "dp-vs-code-rn-fast-file-generator". After writing up a brief description, we recommend including the following sections.
+This Visual Studio Code extension simplifies the process of creating new screens and views for React Native applications. It allows you to quickly generate boilerplate code for a screen and its corresponding view, including the necessary imports and basic structure.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- Generate new screens and views with a single command.
+- Automatically create `Screen.tsx` and `View.tsx` files with predefined templates.
+- Choose to create files in the root of the `screens` and `views` folders or within a specific subfolder.
+- Option to create a new subfolder if it doesn't already exist.
+- Check for existing files and prompt to overwrite if necessary.
 
-For example if there is an image subfolder under your extension project workspace:
+## Usage
 
-\!\[feature X\]\(images/feature-x.png\)
+To use the extension, follow these steps:
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+1. Open the Command Palette (`Ctrl+Shift+P` on Windows/Linux, `Cmd+Shift+P` on macOS).
+2. Type `Add Screen and View` and select the command.
+3. Enter the name for the new screen and view when prompted.
+4. Select a subfolder or choose to create a new one. You can also select the root option to create files directly in the root of the `screens` and `views` folders.
+5. The extension will generate the files and open them in the editor.
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+This extension assumes that your project has a `src` folder with `screens` and `views` subfolders. If the `src` folder does not exist, the extension will show an error message.
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+This extension does not have any configurable settings at the moment.
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Currently, there are no known issues. If you encounter any problems, please open an issue on the [GitHub repository](#).
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
+Initial release of the Screen and View Generator extension.
 
-### 1.0.1
+## Contributing
 
-Fixed issue #.
+Contributions are welcome! If you would like to contribute to this extension, please fork the repository and submit a pull request.
 
-### 1.1.0
+## License
 
-Added features X, Y, and Z.
+This extension is licensed under the [MIT License](LICENSE).
 
 ---
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -6,71 +6,194 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     'extension.addScreenAndView',
     async (uri: vscode.Uri) => {
+      // Prompt for the screen/view name
       const name = await vscode.window.showInputBox({
         prompt: 'Enter the name for the new screen and view',
       })
+
+      if (!uri) {
+        const workspaceFolders = vscode.workspace.workspaceFolders
+        if (!workspaceFolders) {
+          vscode.window.showErrorMessage('No workspace is open.')
+          return
+        }
+        // Use the first workspace folder and append the 'src' folder to it
+        const workspaceRoot = workspaceFolders[0].uri.fsPath
+        uri = vscode.Uri.file(path.join(workspaceRoot, 'src'))
+      }
 
       if (!name) {
         // User did not enter a name, exit the command
         return
       }
 
-      // Assuming uri is the path to the 'src' folder
-      const screenFolder = path.join(uri.fsPath, 'screens')
-      const viewFolder = path.join(uri.fsPath, 'views')
+      // Get the list of subfolders in 'screens' and 'views'
+      const screenSubfolders = getSubfolders(path.join(uri.fsPath, 'screens'))
+      const viewSubfolders = getSubfolders(path.join(uri.fsPath, 'views'))
 
-      // Create the folders if they don't exist
+      // Combine the lists and add an option to create a new subfolder
+      const subfolders = Array.from(
+        new Set([...screenSubfolders, ...viewSubfolders])
+      )
+      subfolders.sort() // Sort the list alphabetically
+      subfolders.unshift("Root of 'screens/views'") // Add option for the root
+      subfolders.unshift('Create new subfolder...') // Add option to create a new subfolder at the beginning
+
+      // Show quick pick for subfolders
+      const subfolder = await vscode.window.showQuickPick(subfolders, {
+        placeHolder: 'Select a subfolder or create a new one',
+      })
+
+      if (!subfolder) {
+        // User did not select a subfolder, exit the command
+        return
+      }
+
+      let finalSubfolder = ''
+      if (subfolder === 'Create new subfolder...') {
+        // Prompt user to enter a new subfolder name
+        finalSubfolder = await vscode.window.showInputBox({
+          prompt: 'Enter the name for the new subfolder',
+        })
+
+        if (!finalSubfolder) {
+          // User did not enter a subfolder name, exit the command
+          return
+        }
+      } else if (subfolder !== "Root of 'screens/views'") {
+        // If the user did not select the root, use the selected subfolder
+        finalSubfolder = subfolder
+      }
+
+      // Construct the folder paths
+      const screenFolder = path.join(uri.fsPath, 'screens', finalSubfolder)
+      const viewFolder = path.join(uri.fsPath, 'views', finalSubfolder)
+
+      // Create the subfolders if they don't exist
       if (!fs.existsSync(screenFolder)) {
-        fs.mkdirSync(screenFolder)
+        fs.mkdirSync(screenFolder, { recursive: true })
       }
       if (!fs.existsSync(viewFolder)) {
-        fs.mkdirSync(viewFolder)
+        fs.mkdirSync(viewFolder, { recursive: true })
       }
 
       // Define file paths
       const screenFilePath = path.join(screenFolder, `${name}Screen.tsx`)
       const viewFilePath = path.join(viewFolder, `${name}View.tsx`)
 
+      // Check if the files already exist
+      const screenFileExists = fs.existsSync(screenFilePath)
+      const viewFileExists = fs.existsSync(viewFilePath)
+
+      // Ask the user for confirmation if either file exists
+      if (screenFileExists || viewFileExists) {
+        const overwrite = await vscode.window.showWarningMessage(
+          `The file${screenFileExists && viewFileExists ? 's' : ''} ${
+            screenFileExists ? `${name}Screen.tsx` : ''
+          }${screenFileExists && viewFileExists ? ' and ' : ''}${
+            viewFileExists ? `${name}View.tsx` : ''
+          } already exist${
+            screenFileExists && viewFileExists ? '' : 's'
+          }. Do you want to overwrite?`,
+          { modal: true },
+          'Yes',
+          'No'
+        )
+
+        if (overwrite !== 'Yes') {
+          // User chose not to overwrite the existing file(s)
+          vscode.window.showInformationMessage('Operation cancelled.')
+          return
+        }
+      }
+
+      // Get the workspace root path
+      const workspaceRoot = vscode.workspace.workspaceFolders
+        ? vscode.workspace.workspaceFolders[0].uri.fsPath
+        : null
+
+      // Create a relative path from the workspace root
+      const relativeScreenPath = path.relative(workspaceRoot, screenFilePath)
+      const relativeViewPath = path.relative(workspaceRoot, viewFilePath)
+
+      // Transform the paths to the desired format
+      const transformedScreenPath = transformPath(relativeScreenPath)
+      const transformedViewPath = transformPath(relativeViewPath)
+
       // Define file contents
-      const screenContent = `import React from 'react';
-import { View, Text } from 'react-native';
-import { ${name} } from '~/views/${name}View'
+      const screenContent = `import React from 'react'
+import { ${name}View } from '${transformedViewPath}'
 
 const ${name}Screen = () => {
-  return (
-    <${name}View />
-  );
-};
+  return <${name}View />
+}
 
-export default ${name}Screen;
+export default ${name}Screen
 `
 
-      const viewContent = `import React from 'react';
-import { View, Text } from 'react-native';
+      const viewContent = `import React from 'react'
+import { View, Text } from 'react-native'
+import styled from 'styled-components'
 
-const ${name}View = () => {
+export const ${name}View = () => {
   return (
     <View>
       <Text>${name} View</Text>
     </View>
-  );
-};
-
-export default ${name}View;
+  )
+}
 `
 
       // Write files to disk
       fs.writeFileSync(screenFilePath, screenContent)
       fs.writeFileSync(viewFilePath, viewContent)
 
+      // Open the created files in the editor
+      const screenUri = vscode.Uri.file(screenFilePath)
+      const viewUri = vscode.Uri.file(viewFilePath)
+
+      // Open the Screen file and then the View file
+      const screenDocument = await vscode.window.showTextDocument(screenUri, {
+        preview: false,
+      })
+      vscode.window.showTextDocument(viewUri, {
+        preview: false,
+        viewColumn: vscode.ViewColumn.Beside,
+      })
+
       // Show a message to the user
       vscode.window.showInformationMessage(
-        `Files created: ${name}Screen.tsx and ${name}View.tsx`
+        `Files created: ${path.join(
+          finalSubfolder,
+          `${name}Screen.tsx`
+        )} and ${path.join(finalSubfolder, `${name}View.tsx`)}`
       )
     }
   )
 
   context.subscriptions.push(disposable)
+}
+
+// Helper function to get subfolders within a given folder
+function getSubfolders(folderPath: string): string[] {
+  if (!fs.existsSync(folderPath)) {
+    return []
+  }
+
+  return fs
+    .readdirSync(folderPath, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
+}
+
+// Helper function to transform the path to the desired format
+function transformPath(filePath: string): string {
+  // Remove the file extension
+  let withoutExtension = filePath.replace(/\.[^/.]+$/, '')
+  // Remove the 'src/' prefix
+  withoutExtension = withoutExtension.replace(/^src\//, '')
+  // Prepend with '~/'
+  return `~/${withoutExtension}`
 }
 
 export function deactivate() {}
